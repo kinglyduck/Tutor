128df50afe50a6458dc7d475e9cf9502c7039d87
Brody Gimson <knuckles1006@gmail.com>
2017-01-11 14:55:01 -0800

    Added scripts folder and start of border script

---
+++ Assets/Assets/Scripts/BorderCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCollision : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+
+		if(col.gameObject.name == "bubble") {
+
+
+		}
+	}
+}
